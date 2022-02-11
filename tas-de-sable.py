11f2a5d0f0a9a815af0df13bbd4d7697f667e86e
Mathis ALLOUCHE <mathis.allouche@ens.uvsq.fr>
2022-02-11 15:36:05 +0100

    Début projet

--- tas-de-sable.py
+++ tas-de-sable.py
@@ -1,5 +1,42 @@
+# INFORMATION GROUPE
 # GROUPE MI TD 03
 # Mathis ALLOUCHE
 # jennifer said
 # xavier koubonou
 # https://github.com/uvsq22104691/tas-de-sable
+
+
+# Import
+import tkinter as tk
+# from random import randint
+
+
+# Constante
+
+# largeur du canvas
+W = 600
+# hauteur du canvas
+H = 600
+
+
+# Variable globale
+
+
+# Fonction
+
+
+# Création widgets
+root = tk.Tk()
+root.title("Projet - tas de sable")
+
+canvas = tk.Canvas(root, width=W, height=H, bg="blue")
+
+# Placement widgets
+canvas.grid(row=0, column=0)
+
+
+# Evenements widgets
+
+
+# Boucle principale
+root.mainloop()
