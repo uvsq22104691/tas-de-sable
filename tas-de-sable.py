0026b39a29a7931347bb5b7e7d2dd2d51c3f50c1
Mathis ALLOUCHE <mathis.allouche@ens.uvsq.fr>
2022-02-11 21:45:11 +0100

    avalanche

--- tas-de-sable.py
+++ tas-de-sable.py
@@ -20,9 +20,30 @@ H = 600
 
 
 # Variable globale
+grille = [
+    [4, 5, 2],
+    [4, 3, 0],
+    [1, 5, 4]
+]
+
+# grille = [[randint(0, 6) for _ in range(3)] for _ in range(3)]
 
 
 # Fonction
+def avalanche():
+    global grille
+    n = len(grille)
+    grilletmp = grille.copy()
+
+    for i in range(len(grille)):
+        for j in range(len(grille[0])):
+            if grille[i][j] >= 4:
+                grilletmp[i][j] = grille[i][j] - 4
+                for p, q in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    if 0 <= i + p < n and 0 <= j + q < n:
+                        grilletmp[i + p][j + q] += 1
+                        print(grilletmp)
+    grille = grilletmp.copy()
 
 
 # Création widgets
@@ -39,4 +60,7 @@ canvas.grid(row=0, column=0)
 
 
 # Boucle principale
+print(grille)
+avalanche()
+print(grille)
 root.mainloop()
