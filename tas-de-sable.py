4ea4ac21681df0b8eac590c626132f4209e73d34
Mathis ALLOUCHE <mathis.allouche@ens.uvsq.fr>
2022-02-11 23:38:07 +0100

    ajout fonction a coder début interface graphique

--- tas-de-sable.py
+++ tas-de-sable.py
@@ -8,32 +8,51 @@
 
 # Import
 import tkinter as tk
-# from random import randint
+import copy
+import numpy as np
+from random import randint
 
 
 # Constante
 
-# largeur du canvas
+# Largeur du canvas
 W = 600
-# hauteur du canvas
+# Hauteur du canvas
 H = 600
-
+# Dictionnaire de couleur
+COULEUR = {
+    "noir": "000000",
+    "rouge": "#FF0000",
+    "vert": "#00FF00",
+    "bleu": "#0000FF",
+    "cyan": "#FFFF00",
+    "jaune": "#00FFFF",
+    "magenta": "#FF00FF",
+    "blanc": "#FFFFFF",
+}
 
 # Variable globale
-grille = [
-    [4, 5, 2],
-    [4, 3, 0],
-    [1, 5, 4]
-]
+grille = [[randint(0, 6) for _ in range(3)] for _ in range(3)]
 
-# grille = [[randint(0, 6) for _ in range(3)] for _ in range(3)]
+grille = [
+    [1, 0, 4],
+    [0, 1, 4],
+    [0, 1, 2]
+]
 
 
 # Fonction
 def avalanche():
+    ''' avalanche() est une fonction qui transmet en parallèle
+        1 grain de sable à chaque voisin de la grille en croix
+        pour chaque case ayant au moins 4 grains.
+        Elle renvoie le nombre de grain de la case ayant le plus
+        de grain dans la grille
+    '''
     global grille
     n = len(grille)
-    grilletmp = [[v for v in row] for row in grille]
+    grilletmp = copy.deepcopy(grille)
+    grain_max = 0
 
     for i in range(len(grille)):
         for j in range(len(grille[0])):
@@ -42,8 +61,30 @@ def avalanche():
                 for p, q in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                     if 0 <= i + p < n and 0 <= j + q < n:
                         grilletmp[i + p][j + q] += 1
-                print(*grilletmp, sep="\n", end="\n\n")
-    grille = grilletmp.copy()
+
+    grille = copy.deepcopy(grilletmp)
+    grain_max = np.max(grille)
+    return grain_max
+
+
+def dessine_grille():
+    pass
+
+
+def charger_grille():
+    pass
+
+
+def sauvegarder_grille():
+    pass
+
+
+def addition_config():
+    pass
+
+
+def soustration_config():
+    pass
 
 
 # Création widgets
@@ -52,9 +93,14 @@ root.title("Projet - tas de sable")
 
 canvas = tk.Canvas(root, width=W, height=H, bg="blue")
 
+bouton_aléatoire = tk.Button(root, text="Config aléatoire")
+bouton_pile_centre = tk.Button(root, text="Config pile centrée")
 
 # Placement widgets
-canvas.grid(row=0, column=0)
+canvas.grid(row=0, column=1, rowspan=8)
+
+bouton_aléatoire.grid(row=0, column=0)
+bouton_pile_centre.grid(row=1, column=0)
 
 
 # Evenements widgets
@@ -62,7 +108,9 @@ canvas.grid(row=0, column=0)
 
 # Boucle principale
 print(*grille, sep="\n", end="\n\n")
-avalanche()
-avalanche()
-print(*grille, sep="\n", end="\n\n")
+while 1:
+    grain_max = avalanche()
+    print(*grille, sep="\n", end="\n\n")
+    if grain_max < 4:
+        break
 root.mainloop()
