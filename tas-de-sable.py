bdfe6a2ef036b6c773e0a482e57aa543e5acd42d
Mathis ALLOUCHE <mathis.allouche@ens.uvsq.fr>
2022-03-12 11:05:25 +0100

    optimization avalanche menu charger config

--- tas-de-sable.py
+++ tas-de-sable.py
@@ -11,7 +11,7 @@
 # Import
 import tkinter.filedialog as fd
 import tkinter as tk
-import copy
+import marshal
 import os
 from random import randint
 
@@ -24,7 +24,7 @@ W = 600
 H = 600
 
 # Largeur de la grille
-N = 20
+N = 31
 
 # liste de couleurs
 COULEUR = [
@@ -44,6 +44,7 @@ COULEUR = [
 # Variable globale
 G_grille = []
 G_pause = True
+G_auto = True
 
 
 # Fonction
@@ -51,8 +52,9 @@ def init():
     ''' Crée une grille aléatoire et l'affiche
         Vérification et création si besoin des grilles par défaut.
     '''
-    global grille
-    grille = [[randint(0, 8) for _ in range(N)] for _ in range(N)]
+    global G_grille
+    G_grille = [[randint(0, 8) for _ in range(N)] for _ in range(N)]
+    dessine_grille()
 
 
 def avalanche(grille):
@@ -62,7 +64,7 @@ def avalanche(grille):
         Rrenvoie le nombre de grain de la case ayant le plus
         de grain dans la grille (int).
     '''
-    grilletmp = copy.deepcopy(grille)
+    grilletmp = marshal.loads(marshal.dumps(grille))
     voisins = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
     for i in range(N):
@@ -74,11 +76,13 @@ def avalanche(grille):
                 # addition de 1 grain par voisin
                 for p, q in voisins:
                     try:
+                        if i + p == -1 or j + q == -1:
+                            continue
                         grilletmp[i + p][j + q] += 1
                     except IndexError:
                         pass
 
-    grille = copy.deepcopy(grilletmp)
+    grille = marshal.loads(marshal.dumps(grilletmp))
     grain_max = 0
     for i in grille:
         if grain_max < max(i):
@@ -87,19 +91,28 @@ def avalanche(grille):
     return (grille, grain_max)
 
 
-def stabilise(grille):
-    if grille is None:
-        global G_grille
+def stabilise(grille=None, clicked=False):
+    if clicked and (G_pause or G_auto):
+        return
+    global G_grille
+    dessine = False
+
+    if grille is None or grille is G_grille:
         grille = G_grille
+        dessine = True
 
-    while 1:
-        grille, grain_max = avalanche(grille)
-        if grille is G_grille:
-            dessine_grille()
+    grille, grain_max = avalanche(grille)
+    if dessine:
+        G_grille = grille
+        dessine_grille()
 
-        if grain_max < 4:
-            break
-    return grille
+    if grain_max >= 4 and not G_pause and G_auto:
+        root.after(100, stabilise, grille)
+    if grain_max < 4:
+        if not G_pause:
+            change_pause()
+
+    return grille, grain_max
 
 
 def dessine_grille():
@@ -118,13 +131,13 @@ def dessine_grille():
                 fill=COULEUR[G_grille[i][j] if G_grille[i][j] < 11 else 10],
                 width=0
             )
-            canvas.create_text(
-                (W / N) / 2 + (W / N) * j,
-                (H / N) / 2 + (H / N) * i,
-                text=str(G_grille[i][j]),
-                width=50,
-                fill="black" if G_grille[i][j] < 10 else "white"
-            )
+            # canvas.create_text(
+            #     (W / N) / 2 + (W / N) * j,
+            #     (H / N) / 2 + (H / N) * i,
+            #     text=str(G_grille[i][j]),
+            #     width=50,
+            #     fill="black" if G_grille[i][j] < 10 else "white"
+            # )
     canvas.update()
 
 
@@ -135,55 +148,60 @@ def fenetre_charger_config():
 
     # On crée une nouvelle fenetre pour choisir la config à charger
 
-    fen_option = tk.Tk()
-    fen_option.title = "Fenêtre d'option"
+    fen_charge_conf = tk.Tk()
+    fen_charge_conf.title("Fenêtre chargement config")
 
     bouton_aléatoire = tk.Button(
-        fen_option,
-        text="Config aléatoire",
-        command=lambda: charger_config("aleatoire")
+        fen_charge_conf,
+        text="Aléatoire",
+        command=lambda: charger_config("aleatoire", True)
     )
 
     bouton_pile_centre = tk.Button(
-        fen_option,
-        text="Config pile centrée",
-        command=lambda: charger_config("pile_centree")
+        fen_charge_conf,
+        text="Pile centrée",
+        command=lambda: charger_config("pile_centree", True)
+    )
+
+    entry_pile_centre = tk.Entry(
+        fen_charge_conf
     )
 
     bouton_max_stable = tk.Button(
-        fen_option,
-        text="Config max stable",
-        command=lambda: charger_config("max_stable")
+        fen_charge_conf,
+        text="Max stable",
+        command=lambda: charger_config("max_stable", True)
     )
 
     bouton_max_stable = tk.Button(
-        fen_option,
-        text="Config max stable",
-        command=lambda: charger_config("double_max_stable")
+        fen_charge_conf,
+        text="Double max stable",
+        command=lambda: charger_config("double_max_stable", True)
     )
 
     bouton_config_identity = tk.Button(
-        fen_option,
-        text="Config indentity",
-        command=lambda: charger_config("identity")
+        fen_charge_conf,
+        text="Identity",
+        command=lambda: charger_config("identity", True)
     )
 
     bouton_charge_config = tk.Button(
-        fen_option,
-        text="Config indentity",
-        command=lambda: charger_config("identity")
+        fen_charge_conf,
+        text="Fichier",
+        command=lambda: charger_config(None, True)
     )
 
-    bouton_aléatoire.grid(row=0, column=0)
-    bouton_pile_centre.grid(row=1, column=0)
-    bouton_max_stable.grid(row=2, column=0)
-    bouton_config_identity.grid(row=3, column=0)
-    bouton_charge_config.grid(row=5, column=0)
+    bouton_aléatoire.grid(row=0, column=0, pady=20)
+    bouton_pile_centre.grid(row=1, column=0, pady=20)
+    entry_pile_centre.grid(row=1, column=1, padx=35, pady=20)
+    bouton_max_stable.grid(row=2, column=0, padx=35, pady=20)
+    bouton_config_identity.grid(row=3, column=0, pady=20)
+    bouton_charge_config.grid(row=5, column=0, pady=20)
 
-    fen_option.mainloop()
+    fen_charge_conf.mainloop()
 
 
-def charger_config(conf=None):
+def charger_config(conf=None, replace_G_grille=False):
     if conf is None:
         f = fd.askopenfile(
             initialdir=os.getcwd() + "/config/",
@@ -196,28 +214,48 @@ def charger_config(conf=None):
         global N
         g = eval(f.readline())
         N = len(g)
-        return g
+        config = g
 
-    if conf == "aleatoire":
-        return [[randint(0, 10) for _ in range(N)] for _ in range(N)]
+    elif conf == "aleatoire":
+        config = [[randint(0, 10) for _ in range(N)] for _ in range(N)]
 
     elif conf == "pile_centree":
         grilletmp = [[0] * N for _ in range(N)]
         nb_grains = 10
-        grilletmp[N // 2][N // 2] = nb_grains
-        return grilletmp
+        if N % 2 == 0:
+            grilletmp[(N // 2) - 1][(N // 2) - 1] = nb_grains
+            grilletmp[(N // 2) - 1][N // 2] = nb_grains
+            grilletmp[N // 2][(N // 2) - 1] = nb_grains
+            grilletmp[N // 2][N // 2] = nb_grains
+        else:
+            grilletmp[N // 2][N // 2] = nb_grains
+        config = grilletmp
 
     elif conf == "max_stable":
-        return [[3] * N for _ in range(N)]
+        config = [[3] * N for _ in range(N)]
 
     elif conf == "double_max_stable":
-        return [[6] * N for _ in range(N)]
+        config = [[6] * N for _ in range(N)]
 
     elif conf == "identity":
         g1 = charger_config("double_max_stable")
-        g1 = stabilise(g1)
 
-        # return soustration_config(g1, )
+        g2, grain_max = avalanche(g1)
+        while grain_max >= 4:
+            g2, grain_max = avalanche(g2)
+        config = soustration_config(g1, g2)
+
+        config, grain_max = avalanche(config)
+        while grain_max >= 4:
+            config, grain_max = avalanche(config)
+
+    if replace_G_grille:
+        global G_grille
+        G_grille = config
+        dessine_grille()
+        if not G_pause:
+            change_pause()
+    return config
 
 
 def sauvegarder_config():
@@ -243,7 +281,7 @@ def sauvegarder_config():
         return
 
     with open(fichier, "w") as f:
-        f.write(str(grille).replace(' ', ''))
+        f.write(str(G_grille).replace(' ', ''))
         f.close()
 
 
@@ -259,7 +297,7 @@ def addition_config(g1=None, g2=None):
 
         return g1
 
-    global grille
+    global G_grille
     grille2 = charger_config()
 
     n = len(grille2)
@@ -268,7 +306,7 @@ def addition_config(g1=None, g2=None):
 
     for i in range(n):
         for j in range(n):
-            grille[i][j] += grille2[i][j]
+            G_grille[i][j] += grille2[i][j]
 
     dessine_grille()
 
@@ -287,7 +325,7 @@ def soustration_config(g1=None, g2=None):
 
         return g1
 
-    global grille
+    global G_grille
     grille2 = charger_config()
 
     n = len(grille2)
@@ -296,32 +334,90 @@ def soustration_config(g1=None, g2=None):
 
     for i in range(n):
         for j in range(n):
-            grille[i][j] -= grille2[i][j]
-            if grille[i][j] < 0:
-                grille[i][j] = 0
+            G_grille[i][j] -= grille2[i][j]
+            if G_grille[i][j] < 0:
+                G_grille[i][j] = 0
+    dessine_grille()
 
 
 def change_pause():
     ''' Met en pause ou reprend les avalanches.'''
-    # bouton_pause["text"]
+    global G_pause
+    bouton_pause['text'] = "Pause" if bouton_pause['text'] == "Reprendre" else "Reprendre"
+    G_pause = not G_pause
+    if not G_pause and G_auto:
+        stabilise(G_grille)
 
 
 def change_affichage():
     ''' Change la façon dont les avalanches sont appelé.
-        ciné : auto avec x secondes d'intervalles.
+        cinéma : auto avec x secondes d'intervalles.
         clique : manuel une avalanche par clique.
-        '''
-    pass
+    '''
+    global G_auto
+    bouton_cinema['text'] = "Cinéma" if bouton_cinema['text'] == "Clique" else "Clique"
+    G_auto = not G_auto
+    if not G_pause:
+        change_pause()
+
+
+def change_options():
+    fen_change_opt = tk.Tk()
+    fen_change_opt.title("Fenêtre d'options")
+
+    bouton_aléatoire = tk.Button(
+        fen_change_opt,
+        text="Aléatoire",
+        command=lambda: charger_config("aleatoire", True)
+    )
+
+    bouton_pile_centre = tk.Button(
+        fen_change_opt,
+        text="Pile centrée",
+        command=lambda: charger_config("pile_centree", True)
+    )
+
+    bouton_max_stable = tk.Button(
+        fen_change_opt,
+        text="Max stable",
+        command=lambda: charger_config("max_stable", True)
+    )
+
+    bouton_max_stable = tk.Button(
+        fen_change_opt,
+        text="Double max stable",
+        command=lambda: charger_config("double_max_stable", True)
+    )
+
+    bouton_config_identity = tk.Button(
+        fen_change_opt,
+        text="Identity",
+        command=lambda: charger_config("identity", True)
+    )
+
+    bouton_charge_config = tk.Button(
+        fen_change_opt,
+        text="Fichier",
+        command=lambda: charger_config(None, True)
+    )
+
+    bouton_aléatoire.grid(row=0, column=0, pady=20)
+    bouton_pile_centre.grid(row=1, column=0, pady=20)
+    bouton_max_stable.grid(row=2, column=0, padx=35, pady=20)
+    bouton_config_identity.grid(row=3, column=0, pady=20)
+    bouton_charge_config.grid(row=5, column=0, pady=20)
+
+    fen_change_opt.mainloop()
 
 
 # Création widgets
 root = tk.Tk()
 root.title("Projet - tas de sable")
 
-canvas = tk.Canvas(root, width=W, height=H, bg="blue")
-bouton_cinema = tk.Button(root, text="cinéma", command=change_affichage)
-bouton_pause = tk.Button(root, text="pause", command=change_pause)
-bouton_option = tk.Button(root, text="option")
+canvas = tk.Canvas(root, width=W, height=H, bg="white")
+bouton_cinema = tk.Button(root, text="Cinéma", command=change_affichage)
+bouton_pause = tk.Button(root, text="Reprendre", command=change_pause)
+bouton_option = tk.Button(root, text="Options", command=change_options)
 
 bouton_sauvegarder_config = tk.Button(
     root,
@@ -356,23 +452,12 @@ bouton_option.grid(row=2, column=0)
 bouton_sauvegarder_config.grid(row=3, column=0)
 bouton_charger_config.grid(row=4, column=0)
 bouton_addition_config.grid(row=5, column=0)
-bouton_soustraction_config.grid(row=6, column=0)
+bouton_soustraction_config.grid(row=6, column=0, padx=20)
 
 
 # Evenements widgets
-
+root.bind('<space>', lambda e: stabilise(None, True))
 
 # Boucle principale
 init()
-
-G_grille = charger_config()
-dessine_grille()
-
-
-while 1:
-    G_grille, grain_max = avalanche(G_grille)
-    dessine_grille()
-    if grain_max < 4:
-        break
-
 root.mainloop()
